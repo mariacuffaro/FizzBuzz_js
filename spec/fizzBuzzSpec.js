61be2e03d1465791c6c037ff6646826236de7236
Student <student@Makerss-MacBook-Pro-2.local>
2019-09-17 15:27:09 +0100

    refactored and added console check

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,26 +1,31 @@
 describe("FizzBuzz", function() {
+  var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
 
   it('should return FizzBuzz if given multiple of 3 and 5', function() {
-    expect(FizzBuzz(15)).toEqual('FizzBuzz');
-    expect(FizzBuzz(30)).toEqual('FizzBuzz');
-    expect(FizzBuzz(60)).toEqual('FizzBuzz');
+    expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
+    expect(fizzBuzz.play(60)).toEqual('FizzBuzz');
   });
 
   it('should return Fizz if given multiple of 3', function() {
-    expect(FizzBuzz(3)).toEqual('Fizz');
-    expect(FizzBuzz(6)).toEqual('Fizz');
-    expect(FizzBuzz(9)).toEqual('Fizz');
+    expect(fizzBuzz.play(3)).toEqual('Fizz');
+    expect(fizzBuzz.play(6)).toEqual('Fizz');
+    expect(fizzBuzz.play(9)).toEqual('Fizz');
   });
 
   it('should return Buzz if given multiple of 5', function() {
-    expect(FizzBuzz(5)).toEqual('Buzz');
-    expect(FizzBuzz(10)).toEqual('Buzz');
-    expect(FizzBuzz(20)).toEqual('Buzz');
+    expect(fizzBuzz.play(5)).toEqual('Buzz');
+    expect(fizzBuzz.play(10)).toEqual('Buzz');
+    expect(fizzBuzz.play(20)).toEqual('Buzz');
   });
 
   it('should return number if not multiple of 3 is given', function() {
-    expect(FizzBuzz(1)).toEqual(1);
-    expect(FizzBuzz(2)).toEqual(2);
+    expect(fizzBuzz.play(1)).toEqual("1");
+    expect(fizzBuzz.play(2)).toEqual("2");
   });
 
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,12 +1,14 @@
-function FizzBuzz(number){
-  if (number % 3 == 0 && number % 5 == 0) {
-    return 'FizzBuzz';
-  }
-  if (number % 3 == 0) {
-    return 'Fizz';
-  }
-  if (number % 5 == 0) {
-    return 'Buzz';
-  }
-  return number;
+function FizzBuzz(){
+};
+
+FizzBuzz.prototype.play = function(number) {
+  if (number % 3 == 0 && number % 5 == 0) {return 'FizzBuzz';}
+  if (number % 3 == 0) {return 'Fizz';}
+  if (number % 5 == 0) {return 'Buzz';}
+  return number.toString();
+};
+
+var fizzBuzz = new FizzBuzz();
+for(var i=1; i < 100; i++){
+    console.log(fizzBuzz.play(i));
 };
