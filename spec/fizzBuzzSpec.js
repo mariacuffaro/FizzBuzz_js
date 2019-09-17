4eab18ef737217bf170fd53066e60a4ea8d9b50f
Valeria Serci <balerty84@gmail.com>
2019-09-17 14:44:53 +0100

    FizzBuzz works

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,6 +1,27 @@
 describe("FizzBuzz", function() {
-  it('should return 1 if 1 is given', function() {
-    expect(FizzBuzz(1)).toEqual(1);
+
+  it('should return FizzBuzz if given multiple of 3 and 5', function() {
+    expect(FizzBuzz(15)).toEqual('FizzBuzz');
+    expect(FizzBuzz(30)).toEqual('FizzBuzz');
+    expect(FizzBuzz(60)).toEqual('FizzBuzz');
   });
+
+  it('should return Fizz if given multiple of 3', function() {
+    expect(FizzBuzz(3)).toEqual('Fizz');
+    expect(FizzBuzz(6)).toEqual('Fizz');
+    expect(FizzBuzz(9)).toEqual('Fizz');
+  });
+
+  it('should return Buzz if given multiple of 5', function() {
+    expect(FizzBuzz(5)).toEqual('Buzz');
+    expect(FizzBuzz(10)).toEqual('Buzz');
+    expect(FizzBuzz(20)).toEqual('Buzz');
+  });
+
+  it('should return number if not multiple of 3 is given', function() {
+    expect(FizzBuzz(1)).toEqual(1);
+    expect(FizzBuzz(2)).toEqual(2);
+  });
+
 
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,3 +1,12 @@
 function FizzBuzz(number){
+  if (number % 3 == 0 && number % 5 == 0) {
+    return 'FizzBuzz';
+  }
+  if (number % 3 == 0) {
+    return 'Fizz';
+  }
+  if (number % 5 == 0) {
+    return 'Buzz';
+  }
   return number;
 };
