e3dc8a35b26faa47e0519473370a74f2373ffecb
Valeria Serci <balerty84@gmail.com>
2019-09-17 14:24:23 +0100

    First test

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,6 @@
+describe("FizzBuzz", function() {
+  it('should return 1 if 1 is given', function() {
+    expect(FizzBuzz(1)).toEqual(1);
+  });
+
+});
---
+++ src/fizzBuzz.js
@@ -0,0 +1,3 @@
+function FizzBuzz(number){
+  return number;
+};
